ee881ad9c3d9e621515d23c5f6eca7472a376f71
Javanshir <cavansir.agayev25@outlook.com>
2024-06-28 16:50:56 +0400

    addBook method has been succesfully created.

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,30 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        addBook("978-0061120084", "To Kill a Mockingbird", "Harper Lee", "Classic American novel");
+        addBook("978-0451524935",  "1984", "George Orwell","Dystopian political fiction");
+        addBook("978-0743273565",  "The Great Gatsby","F. Scott Fitzgerald", "Roaring twenties tragedy");
+
+
+        System.out.println("\nAvailable Book Quantity: " + bookQuantity + "\n");
+        for(int i = 0; i< books.length; i++){
+            for(int j = 0; j < books[0].length; j++){
+                System.out.println(books[i][j]);
+            }
+        }
+
+    }
+
+    static void addBook(String ISBN, String title, String author, String additionalInformation){
+        if(bookQuantity < INDEX){
+            books[bookQuantity][0] = ISBN;
+            books[bookQuantity][1] = title;
+            books[bookQuantity][2] = author;
+            books[bookQuantity][3] = additionalInformation;
+
+            bookQuantity++;
+        }else{
+            System.out.println("\nOur shelves are full, so that we can't add the new one...");
+        }
     }
 }
