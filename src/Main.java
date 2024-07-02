8bb024f1ab2671b6461c7764af83aaf880af8489
Javanshir <cavansir.agayev25@outlook.com>
2024-07-02 19:42:18 +0400

    viewAvailableBooks method has been succesfully created

--- src/Main.java
+++ src/Main.java
@@ -16,21 +16,20 @@ public class Main {
     static void viewAvailableBooks() {
         boolean bookFound = false;
 
-
         if (bookQuantity != 0) {
             bookFound = true;
-            System.out.println("Here is the list of all available books in the library.");
+            System.out.println("Here is the list of all available books in the library  ->");
+
             for (int i = 0; i < bookQuantity; i++) {
 
-                System.out.println("\nBook " + (i + 1) + "->");
+                System.out.println("\nBook " + (i + 1) + ":");
                 System.out.println("Title: " + books[i][1]);
                 System.out.println("ISBN: " + books[i][0]);
                 System.out.println("Author: " + books[i][2]);
                 System.out.println("Additional Information: " + books[i][3]);
-
             }
         }
-        if(!bookFound){
+        if (!bookFound) {
             System.out.println("Oops, sth wrong happened while finding books.");
         }
     }
