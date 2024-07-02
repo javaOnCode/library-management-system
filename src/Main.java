e6ddec2107c1756c4e45906d95a3e8e35216c695
Javanshir <cavansir.agayev25@outlook.com>
2024-07-02 19:35:02 +0400

    .

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,28 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    static void viewAvailableBooks() {
+        boolean bookFound = false;
+
+
+        if (bookQuantity != 0) {
+            bookFound = true;
+            System.out.println("Here is the list of all available books in the library.");
+            for (int i = 0; i < bookQuantity; i++) {
+
+                System.out.println("\nBook " + (i + 1) + "->");
+                System.out.println("Title: " + books[i][1]);
+                System.out.println("ISBN: " + books[i][0]);
+                System.out.println("Author: " + books[i][2]);
+                System.out.println("Additional Information: " + books[i][3]);
+
+            }
+        }
+        if(!bookFound){
+            System.out.println("Oops, sth wrong happened while finding books.");
+        }
     }
 }
