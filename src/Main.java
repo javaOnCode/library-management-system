88926b712aba1920cccf915a1d1719c64aed3096
Javanshir <cavansir.agayev25@outlook.com>
2024-07-03 18:05:11 +0400

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,15 +18,11 @@ public class Main {
 
         if (bookQuantity != 0) {
             bookFound = true;
-            System.out.println("Here is the list of all available books in the library  ->");
+            System.out.println("\nHere is the list of all available books in the library  ->\n");
 
             for (int i = 0; i < bookQuantity; i++) {
-
-                System.out.println("\nBook " + (i + 1) + ":");
-                System.out.println("Title: " + books[i][1]);
-                System.out.println("ISBN: " + books[i][0]);
-                System.out.println("Author: " + books[i][2]);
-                System.out.println("Additional Information: " + books[i][3]);
+                System.out.print("Book " + (i + 1) + ": ");
+                System.out.println(books[i][1]);
             }
         }
         if (!bookFound) {
