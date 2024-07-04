0deee1c95b057d2c9bb3af14bf7b36cb7c87140c
Javanshir <cavansir.agayev25@outlook.com>
2024-07-04 20:34:09 +0400

    .

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,22 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    static void extendBooksArrayOnAddition(String newBookISBN, String newBookTitle, String newBookAuthor, String newBookAdditionalInformation) {
+        String[][] newBooks = new String[books.length + 1][4];
+
+        System.arraycopy(books, 0, newBooks, 0, books.length);
+        newBooks[books.length][0] = newBookISBN;
+        newBooks[books.length][1] = newBookTitle;
+        newBooks[books.length][2] = newBookAuthor;
+        newBooks[books.length][3] = newBookAdditionalInformation;
+
+        books = newBooks;
+
+        for (String[] book : books) {
+            System.out.println(book);
+        }
     }
 }
