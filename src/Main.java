28d115afdeaa1783a2ecee7444d259791d81a4df
Javanshir <cavansir.agayev25@outlook.com>
2024-07-05 13:28:22 +0400

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-//        addBook("978-0061120084", "To Kill a Mockingbird", "Harper Lee", "Classic American novel");
-//        addBook("978-0451524935", "1984", "George Orwell", "Dystopian political fiction");
-//        addBook("978-0743273565", "The Great Gatsby", "F. Scott Fitzgerald", "Roaring twenties tragedy");
-//
-//
-//        System.out.println("\nAvailable Book Quantity: " + bookQuantity + "\n");
-//
-//        for (int i = 0; i < bookQuantity; i++) {
-//            System.out.println("Book " + (i + 1) + ": ");
-//            System.out.println("Title: " + books[i][1]);
-//            System.out.println("ISBN: " + books[i][0]);
-//            System.out.println("Author: " + books[i][2]);
-//            System.out.println("Additional Information: " + books[i][3] + "\n");
-//        }
     }
 
     static void addBook(String ISBN, String title, String author, String additionalInformation) {
